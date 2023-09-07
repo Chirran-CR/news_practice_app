dd8ce2884077a948f575d20654e91ad6aa972923
Chirran <swainchittaranjan345@gmail.com>
2023-09-07 20:49:23 +0530

    model for newsArticle is created

--- lib/models/news_article.dart
+++ lib/models/news_article.dart
@@ -5,9 +5,15 @@ class NewsArticle{
   final String url;
   final String urlToImage;
 
-  // NewsArticle({this.title, this.description, this.url, this.urlToImage});
+  // NewsArticle({required this.title, required this.description, required this.url, required this.urlToImage});
   // NewsArticle(this.title, this.description, this.url, this.urlToImage);
-  NewsArticle(String t, String d, String u, String ur): title= t, description= d, url = u, urlToImage = ur;
-  
+  NewsArticle(String t, String d, String u, String ur) : title= t, description= d, url = u, urlToImage = ur;
 
+
+}
+
+class Temnp{
+
+  NewsArticle obj = NewsArticle("dd", "dd", "dd", "dd");
+  // NewsArticle obj2 = NewsArticle(title:"vsal", description:"thsi", url:"this" ,urlToImage:"this");
 }
