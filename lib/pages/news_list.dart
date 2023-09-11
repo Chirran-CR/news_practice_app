e36ddb381fe3944f9748817aee5b4e3c3d86e0c4
Chirran <swainchittaranjan345@gmail.com>
2023-09-11 22:54:57 +0530

    updated images and assets

--- lib/pages/news_list.dart
+++ lib/pages/news_list.dart
@@ -3,9 +3,6 @@ import "package:news_app_practice/viewModels/news_article_list_view_model.dart";
 import "package:provider/provider.dart";
 
 class NewsList extends StatelessWidget {
-  final NewsArticleListViewModel _newsArticleListViewModel =
-      NewsArticleListViewModel();
-
   // NewsList({super.key}) {
   //   _newsArticleListViewModel.populateTopHeadlines();
   // }
@@ -15,7 +12,10 @@ class NewsList extends StatelessWidget {
     // final nvm = Provider.of<NewsArticleListViewModel>(context, listen: true);
     // print("val of nvm.articles is: ${nvm.articles.length}");
     return Scaffold(
-      appBar: AppBar(title: const Text("Latest News")),
+      appBar: AppBar(
+        title: const Text("Latest News"),
+        backgroundColor: Colors.blueAccent,
+      ),
       body: Consumer<NewsArticleListViewModel>(
         builder: (context, value, child) {
           // print("vla of value is ${value.articles}");
@@ -23,8 +23,35 @@ class NewsList extends StatelessWidget {
             itemCount: value.articles.length,
             itemBuilder: (context, index) {
               // print("vla ${value.articles[index].title}");
+              final singleArticle = value.articles[index];
+              final String articleTitle = singleArticle.title.length > 45
+                  ? "${singleArticle.title.substring(0, 45)}..."
+                  : singleArticle.title;
               return ListTile(
-                title: Text(value.articles[index].title),
+                contentPadding: const EdgeInsets.all(0),
+                leading: SizedBox(
+                  height: 100,
+                  width: 100,
+                  // padding: const EdgeInsets.all(0),
+                  // margin: const EdgeInsets.all(0),
+                  child: singleArticle.imageURL == null
+                      ? Image.asset(
+                          "assets/images/news_background.jpeg",
+                          // scale: 1.5,
+                        )
+                      : Image.network(
+                          singleArticle.imageURL!,
+                          // scale: 1.5,
+                        ),
+                ),
+                // title: Text(singleArticle.title),
+                title: Text(
+                  articleTitle,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    // fontWeight
+                  ),
+                ),
               );
             },
           );
